fded319237af920989d725c001fc9c590650f7ae
coecomber <joostvherwaarden@hotmail.com>
2020-01-03 15:05:16 +0100

    Initial commit

---
+++ src/main/java/com/Herwaarden/HighScores/HighScoresApplication.java
@@ -0,0 +1,13 @@
+package com.Herwaarden.HighScores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighScoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HighScoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Herwaarden/HighScores/HighScoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Herwaarden.HighScores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighScoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
